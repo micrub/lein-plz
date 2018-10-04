1b16f8d7dd05ab56e643a6722b0ea25d2b0e0eac
Michael Rubanov <mrubanov-rubanov@protonmail.com>
2018-10-04 03:12:49 +0300

    @version @patch @temp @private @release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-plz "0.3.33-SNAPSHOT"
+(defproject micrub/lein-plz "0.3.33"
   :description "A Leiningen plugin for adding dependencies to projects quickly."
   :url "http://johnwalker.io/"
   :license {:name "Eclipse Public License"
